852475b0c523e1b58bc9cd64185baf1a36561909
zzh <15372971+zhangzehao123@user.noreply.gitee.com>
2025-01-15 19:59:23 +0800

    123123

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!123123");
+        System.out.println("Hello, World!");
     }
 } 
